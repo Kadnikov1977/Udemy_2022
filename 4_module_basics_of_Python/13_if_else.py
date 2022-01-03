539491c2bb0a0f8107deede9417229bd3051d59b
HarryChpoker <43623@mail.ru>
2022-01-03 22:14:23 +0400

    ended 4_module 13_if_else.py

---
+++ 4_module_basics_of_Python/13_if_else.py
@@ -0,0 +1,12 @@
+s = int(input('Enter any number '))
+if s == 7:
+    print('7 is a lucky number! Today is your lucky day!')
+else:
+    print('Thank you! Have a nice day!')
+
+t = int(input('Enter any number '))
+if t % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
+
