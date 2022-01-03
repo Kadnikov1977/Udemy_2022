b8555034561b3c2398d8e26b315d9123ea3103f4
HarryChpoker <43623@mail.ru>
2022-01-03 20:24:44 +0400

    ended 4 module

---
+++ 4_module/create_variable.py
@@ -0,0 +1,20 @@
+# car1_vendor = "Mitsubishi"
+# car1_model = "Montero Sport"
+# car1_color = "white"
+# car1_year = 2003
+# car1_numder_of_door = 5
+# print(car1_vendor, car1_model, car1_color, car1_year, car1_numder_of_door, sep='\n')
+#
+# car1_color = "red"
+# print(car1_vendor, car1_model, car1_color, car1_year, car1_numder_of_door, sep='\n')
+
+# ===========================================================
+year1 = 23
+year2 = 32
+year3 = 56
+year4 = 45
+
+mean_summ = (year1 + year2 + year3 + year4) / 4
+print(mean_summ)
+
+
