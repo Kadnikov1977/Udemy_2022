e19154e2980284b05a68900f57d724ea7edd84da
HarryChpoker <43623@mail.ru>
2022-01-03 21:45:07 +0400

    ended 4_module 9_Dictionaries.py

---
+++ 4_module_basics_of_Python/9_Dictionaries.py
@@ -0,0 +1,10 @@
+dist = {'HarryChpoker': 1000, 'HarryPoter': 500, 'Garrypoter': 300}
+print(dist)
+print(dist['HarryChpoker'])
+
+comp = {
+    'CPU': 'i7',
+    'RAM': 16000,
+    'HDD': 2000,
+    'GPU': 'GTX1080Ti'}
+print(comp)
