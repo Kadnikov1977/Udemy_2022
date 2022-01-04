66630698154cec8409c6d50d8ec6d8b172880fca
HarryChpoker <43623@mail.ru>
2022-01-04 19:40:48 +0400

    ended 5_module 18_args_kwargs_map_lambda.py.py

--- 5_module_functions/18_args_kwargs_map_lambda.py
+++ 5_module_functions/18_args_kwargs_map_lambda.py
@@ -17,15 +17,25 @@
 
 #================================================================
 
-def your_favorite_color(my_color, **kwargs):
-    if my_color > kwargs['age']:
-        return 'Агай'
+# def your_favorite_color(my_color, **kwargs):
+#     if my_color > kwargs['age']:
+#         return 'Агай'
+#     else:
+#         return 'Щегол'
+#
+# e = your_favorite_color(43, firstname = 'Harry', secondname = 'Chpoker', age = 44)
+#
+# print(e)
+
+#================================================================
+
+def is_ogg(x):
+    if x % 2 == 0:
+        return 0
     else:
-        return 'Щегол'
+        return 1
 
-e = your_favorite_color(43, firstname = 'Harry', secondname = 'Chpoker', age = 44)
+list1 = range(1, 100)
 
-print(e)
-
-
+print(list(map(is_ogg, list1)))
 
