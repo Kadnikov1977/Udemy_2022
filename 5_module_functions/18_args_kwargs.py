b456cf510cb97ad9343ffc4346ed334a28c2a9e7
HarryChpoker <43623@mail.ru>
2022-01-04 19:28:13 +0400

    ended 5_module 18_args_kwargs.py

--- 5_module_functions/18_args_kwargs.py
+++ 5_module_functions/18_args_kwargs.py
@@ -1,16 +1,31 @@
-def is_cat_here(*args):
-    list1 = [str(i).lower() for i in list(args)]
-    if 'cat' in list1:
-        return True
+# def is_cat_here(*args):
+#     list1 = [str(i).lower() for i in list(args)]
+#     if 'cat' in list1:
+#         return True
+#     else:
+#         return False
+#
+# print(is_cat_here('Harry', 'Cat', 'Chpoker'))
+#
+# def is_item_here(item, *args):
+#     if item in list(args):
+#         return True
+#     else:
+#         return False
+#
+# print(is_item_here('qwerty', 'Cat', 'Chpoker', 'qwerty'))
+
+#================================================================
+
+def your_favorite_color(my_color, **kwargs):
+    if my_color > kwargs['age']:
+        return 'Агай'
     else:
-        return False
+        return 'Щегол'
 
-print(is_cat_here('Harry', 'Cat', 'Chpoker'))
+e = your_favorite_color(43, firstname = 'Harry', secondname = 'Chpoker', age = 44)
 
-def is_item_here(item, *args):
-    if item in list(args):
-        return True
-    else:
-        return False
+print(e)
 
-print(is_item_here('qwerty', 'Cat', 'Chpoker', 'qwerty'))
+
+
