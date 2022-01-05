8154bff1365965563111993faef72e22b93c86ae
HarryChpoker <43623@mail.ru>
2022-01-05 23:05:35 +0400

    add read_file.py

---
+++ 8_module_inpun_output/read_file.py
@@ -0,0 +1,6 @@
+text1 = open('sample.txt')
+
+for i in text1:
+    print(i, end='')
+
+text1.close()
---
+++ 8_module_inpun_output/read_file_without_close.py
@@ -0,0 +1,5 @@
+with open('sample.txt', 'r') as text1:
+    for i in text1:
+        print(i, end='')
+
+
