908f5a41ad4c9ec18bd95ac69403ac25a018ca34
HarryChpoker <43623@mail.ru>
2022-01-07 13:03:12 +0400

    add try_exception.py

--- 8_module_read_write_file/add_write.py
+++ 8_module_read_write_file/add_write.py
@@ -4,3 +4,4 @@ with open('colors.txt', 'a') as file2:
     for i in color:
         print(i, file=file2)
 
+
---
+++ 9_errors/try_exception.py
@@ -0,0 +1,7 @@
+x = 0
+try:
+    c = 5 / x
+    print(c)
+except Exception as e:
+    print(e)
+
