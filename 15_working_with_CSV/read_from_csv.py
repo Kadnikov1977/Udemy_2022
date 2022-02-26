c8263eed7c644bc985014cc8bc592a359d08e462
HarryChpoker <43623@mail.ru>
2022-02-26 20:44:14 +0400

    add read_from_csv.py

--- 15_working_with_CSV/read_from_csv.py
+++ 15_working_with_CSV/read_from_csv.py
@@ -4,5 +4,5 @@ import csv
 with open('cars.csv') as file:
     csv_reader = csv.reader(file)
     for car in csv_reader:
-        print(car)
-
+        # print(car)
+        print(f'{car[1]}    опачки   {car[0]}')
